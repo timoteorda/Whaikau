0a7291b349bfc502d6683b3ec6e100480a0e151d
Timoteo Ruiz de Arcaute <70163745+timoteorda@users.noreply.github.com>
2022-02-19 18:50:30 -0300

    Agregado de Main, Footer y NavBar
    Agregado de Main, Footer y NavBar

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+            <footer>
+                <h3>Contactanos</h3>
+                    <ul>
+                        <li>Teléfono:</li>
+                        <li>Mail:</li>
+                        <li>Dirección:</li>
+                    </ul>  
+            </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+            <main>
+                <h1>Bienvenido a la tienda oficial de Whaikau</h1>        
+            </main>
+    )
+}
+
+export default Main
