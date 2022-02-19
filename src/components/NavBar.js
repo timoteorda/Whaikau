2b48913d131af26ad212e2755e912a0a1eb43a7f
Timoteo Ruiz de Arcaute <timoteorda@gmail.com>
2022-02-19 18:48:45 -0300

    Agregado Footer, Main y NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+            <header>
+                <h1>Whaikau</h1>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </header>
+    )
+}
+
+export default NavBar
