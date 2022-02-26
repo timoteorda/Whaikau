3d4aa48a1d0f5e173239e3916ec8383f9d7d5a80
Timoteo Ruiz de Arcaute <70163745+timoteorda@users.noreply.github.com>
2022-02-26 14:36:06 -0300

    Delete components directory

--- components/ItemListContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ItemListContainer = (greeting) => {
-    return(
-            <h1>Bienvenido {greeting.persona.nombre} a la tienda oficial de Whaikau</h1>               
-    )
-}
-
-export default ItemListContainer
